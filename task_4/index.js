aaf777c0edf663f1ceddfd9951b749ea13d8625a
Ruslan Salianko <r.salianko@gmail.com>
2021-11-02 11:49:20 +0300

    task_4

--- task_4/index.js
+++ task_4/index.js
@@ -13,21 +13,42 @@
 // Create object => { [key]: [value] } => { name: 'David', lastName: 'Gav', dob: Date(01/01/1990), frient: { name: 'Peter' } }
 // Save data to file data.json
 
+import fs, { readFileSync } from 'fs';
+
+function readFile(file) {
+  const data = readFileSync(file);
+  const str = data.toString();
+  const arr = str
+    .replace(/[\n,\s]/g, '')
+    .split(';');
+  return arr;
+}
+
+function createJson(arr) {
+  let obj = {};
+
+  for (let index = 2; index < arr.length - 1; index++) {
+    if (index % 2 === 0) {
+      if (arr[index].indexOf('.') === -1) {
+        obj[arr[index]] = arr[index + 1];
+      } else {
+        const key = arr[index].split('.');
+        let subObj = {};
+        subObj[key[1]] = arr[index + 1]
+        obj[key[0]] = subObj;
+      }
+    }
+  }
+  console.log(obj)
+  return JSON.stringify(obj);
+}
 
 
-// Web app (Express)
-/** View
- *  - текст ареа (большое текстовое поле), куда вставляется json
- *  - обычное текстовое поле, куда вводится слово.
- *  - область вывода (будет списком)
- */ 
 
-/** Логика
- *  В поле вставляется JSON и вводится значение для поиска.
- *  Запрос отправляется на сервер и на сервере нужно получить список всех полей (путей) в значениях которых есть эта строка
- *  Далее список выводится на страницу
- */
 
-/** Требования
- *  - использовать AJAX
- */
+const arr = readFile('data.csv');
+const json = createJson(arr)
+
+fs.writeFile('data.json', json, (err) => {
+
+});
